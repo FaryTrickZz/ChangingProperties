e0dd4b8938bdf1b09ecc4cd8b98d6269154ce385
Bernardo aparicio cabot <imojino@hotmail.com>
2020-11-10 12:15:15 +0100

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,23 @@
+﻿Class MainWindow
+
+    Private Sub RadioButtonRed_Checked(sender As Object, e As RoutedEventArgs) Handles RadioButtonRed.Checked
+        TextBoxChanging.Background = Brushes.Red
+    End Sub
+
+    Private Sub RadioButtonGreen_Checked(sender As Object, e As RoutedEventArgs) Handles RadioButtonGreen.Checked
+        TextBoxChanging.Background = Brushes.Green
+    End Sub
+
+    Private Sub RadioButtonBlue_Checked(sender As Object, e As RoutedEventArgs) Handles RadioButtonBlue.Checked
+        TextBoxChanging.Background = Brushes.Blue
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        TextBoxText.Text = "TextBox"
+        TextBoxAlt.Text = "TextBox"
+        TextBoxAmple.Text = "TextBox"
+        TextBoxChanging.Text = "Text per defecte"
+
+
+    End Sub
+End Class
